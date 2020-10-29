c9b66f72a576601693896fdbacd928d4653dce68
Steven Sussman <sjsussman@gmail.com>
2020-10-29 13:02:52 -0400

    index changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const server = require("./api/server.js");
 
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 const secret = process.env.SECRET_THING;
 
 console.log(port, secret)
